268bd6ec4dc70cf3bed644c02f737812d6724df3
Danang Adi Nugroho <danangtar@gmail.com>
2022-08-04 23:51:05 +0700

    fix detail

--- src/containers/MovieDetail.js
+++ src/containers/MovieDetail.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-import MovieDetailCard from '../components/MovieDetailCard';
-
+import { Link, Container, Card, CardMedia, Box, CardContent, Typography } from '@mui/material';
 import Tmdb from '../apis/Tmdb';
 
 const MovieDetail = () => {
@@ -36,7 +35,40 @@ const MovieDetail = () => {
         )
 
     return (
-        <MovieDetailCard props={movie}></MovieDetailCard>
+        <Container sx={{ py: 8 }}>
+            <Card sx={{ display: 'flex' }}>
+                
+                <CardMedia
+                    component="img"
+                    sx={{
+                        width: 1/2
+                    }}
+                    image={`${movie.album.images[0].url}`}
+                    alt="Movie poster"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography component="div" variant="h5">
+                        {movie.name}
+                    </Typography>
+                    <Typography component="div" variant="h6">
+                        {movie.artists[0].name}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {new Date(movie.album.release_date).getFullYear()}
+                    </Typography>
+                    <Box
+                        sx={{
+                        width: 175,
+                        display: 'flex',
+                        alignItems: 'center',
+                        }}
+                    >
+                        <Box sx={{ ml: 2 }}>Rating: {movie.popularity}</Box>
+                    </Box>
+                    <Link color="rgb(255, 0, 0)" href={movie.external_urls.spotify}>Open in Spotify</Link>
+                </CardContent>
+            </Card>
+        </Container>
     );
 }
 
